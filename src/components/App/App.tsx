7a789db2a3b8458507f59843355f69da8a41bfd2
Gloria Talavera <gloria.tare@gmail.com>
2024-01-16 11:02:39 +0100

    Create styles of App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "./App.css";
 const App = (): React.ReactElement => {
-  return <div className="container"></div>;
+  return <div className="main-container"></div>;
 };
 
 export default App;
