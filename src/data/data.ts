2e1958030e7f6d1c60593f334a8432c872f5cce6
Gloria Talavera <gloria.tare@gmail.com>
2024-01-20 12:51:48 +0100

    Create simpsonData

---
+++ src/data/data.ts
@@ -0,0 +1,15 @@
+const simpsonData = [
+  {
+    id: "1",
+    name: "Marge Simpson",
+    background:
+      "Marjorie Marge B. Simpson ,  (de soltera Bouvier ; nacida el 19 de marzo  ), es la feliz ama de casa y madre de tiempo completo de la familia Simpson . Con su esposo Homer , tiene tres hijos: Bart , Lisa y Maggie Simpson . Marge es la fuerza moralista en su familia y, a menudo, proporciona una voz fundamental en medio de las payasadas de su familia al tratar de mantener el orden en la casa de los Simpson. Aparte de sus deberes en el hogar, Marge coqueteó brevemente con una serie de carreras que van desde oficial de policía hasta activista contra la violencia.",
+    image:
+      "https://res.cloudinary.com/dglqojivj/image/upload/v1682559694/simpsons/250px-Marge_Simpson_ivadwr.png",
+    gender: "Femenino",
+    status: "Vivo",
+    occupation: "Ama de casa",
+  },
+];
+
+export default simpsonData;
