f2a2f873851743f4d1737baf0c2e3d5de6fa4c62
Gloria Talavera <gloria.tare@gmail.com>
2024-01-20 13:26:42 +0100

    Add loadSimpsons function to HomePage

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,14 @@
+import { useContext, useEffect } from "react";
+import SimpsonsContext from "../store/simpsons/context/SimpsonsContext";
 import Header from "../components/Header/Header";
 
 const HomePage = (): React.ReactElement => {
+  const { loadSimpsons } = useContext(SimpsonsContext);
+
+  useEffect(() => {
+    loadSimpsons();
+  }, [loadSimpsons]);
+
   return <Header />;
 };
 
